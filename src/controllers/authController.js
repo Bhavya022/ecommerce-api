f64722cc236a4bcea24834b9976760ac916747b7
Bhavya022 <dnd88778@gmail.com>
2024-07-29 23:10:38 +0530

    Fix bcrypt deployment issue

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const authController = {
     register: async (req, res) => {
         const { username, email, password } = req.body;
